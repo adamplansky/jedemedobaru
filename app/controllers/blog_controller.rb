aeb45e7b66f56ddabf0ccc56a390793f959d9721
Adam Plansky <adamplansky@gmail.com>
2014-08-28 14:24:19 +0200

    refactor blog_controller

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -18,28 +18,17 @@ class BlogController < ApplicationController
       page = 1
     end
     
-    puts "page: #{page}, page_size: #{PAGE_SIZE}"
-    @allposts
-    if @allposts.nil?
-      @allposts = Array.new
-    end
+    @allposts = Array.new
+    
     @posts = @myClient.posts("jedemedobaru.tumblr.com", limit: PAGE_SIZE, offset: PAGE_SIZE * (page-1) )
-    #puts @posts["posts"]
+    
     @posts["posts"].each_index do |i|
       @allposts[(page-1)*5+i] = @posts["posts"][i]
     end
-    i = 0
-    while i < @allposts.count
-      puts "i[#{i}]: #{@allposts[i]}"
-      i+=1
-    end
-   
-   
-    total_posts         = @posts["total_posts"]
     
-    puts "---------------------------------------------"
-    puts page
-    @allposts              = Kaminari.paginate_array(@allposts, total_count: total_posts).page(page).per(PAGE_SIZE)
+    total_posts = @posts["total_posts"]
+    
+    @allposts = Kaminari.paginate_array(@allposts, total_count: total_posts).page(page).per(PAGE_SIZE)
  
     
   end #end index
